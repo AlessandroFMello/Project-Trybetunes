6a9951cc1959fde4ab691c8b6cef3209635eb386
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-19 16:51:49 -0300

    Requisito 2: Cria um botao e um campo de input e verifica se o botao ativa caso o input tenha 3 ou mais caracteres

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: '',
+      enterButtonDisabled: true,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.activateSaveButton = this.activateSaveButton.bind(this);
+    this.checkNameSize = this.checkNameSize.bind(this);
+  }
+
+  handleInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      id: value,
+    }, this.activateSaveButton);
+  }
+
+  activateSaveButton = () => {
+    if (this.checkNameSize()) {
+      this.setState({ enterButtonDisabled: false });
+    } else {
+      this.setState({ enterButtonDisabled: true });
+    }
+  }
+
+  checkNameSize = () => {
+    const { id } = this.state;
+    const maxLength = 3;
+    if (id.length >= maxLength) {
+      return true;
+    }
+  }
+
   render() {
+    const { enterButtonDisabled } = this.state;
     return (
-      <div data-testid="page-login">
-        Login
+      <div data-testid="page-login" className="page-login">
+        <form>
+          <label htmlFor="login-name">
+            Login
+            <input
+              data-testid="login-name-input"
+              type="text"
+              id="login-name"
+              onChange={ this.handleInputChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ enterButtonDisabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
