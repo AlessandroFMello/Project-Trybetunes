ccf3e2c53a50eb2ca37ddca70be5a547b32a8358
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-19 15:02:24 -0300

    Requisito 1: Adiciona o BrowserRouter e renderiza cada componente com  seu path correspondente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <p>TrybeTunes</p>
-        <p>Teste</p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
