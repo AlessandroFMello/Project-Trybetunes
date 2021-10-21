ba9ca3baa05b80d3985a7b87a921af5436e01824
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-21 15:57:09 -0300

    Renderiza o nome do artista e do album, bem como as musicas do album escolhido

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from './MusicCard';
 
 class Album extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      album: '',
+    };
+
+    this.getAlbumMusics = this.getAlbumMusics.bind(this);
+    this.renderAlbumMusics = this.renderAlbumMusics.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAlbumMusics();
+  }
+
+  getAlbumMusics = async () => {
+    const { match: { params: { id } } } = this.props;
+    const music = await getMusics(id);
+    this.setState({
+      album: music,
+    });
+  }
+
+  renderAlbumMusics = () => {
+    const { album } = this.state;
+    return (
+      <div className="musics">
+        <div>
+          <h2 data-testid="artist-name">{album[0].artistName}</h2>
+          <h3 data-testid="album-name">{album[0].collectionName}</h3>
+          <div>
+            {album.slice(1)
+              .map((element, index) => (
+                <MusicCard
+                  key={ index }
+                  trackName={ element.trackName }
+                  previewUrl={ element.previewUrl }
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
+    const { album } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        { album.length > 0 && this.renderAlbumMusics() }
       </div>
     );
   }
 }
 
 export default Album;
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
